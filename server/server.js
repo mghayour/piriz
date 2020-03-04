b81ff1615d8b14ecf445e97df4e5b437a9c399ce
mghayour <me@mghayour.ir>
2020-03-04 04:12:34 +0330

    work on server side

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@
 const path = require('path')
 const fs = require('fs');
 
-exports.start = function(args) {
+exports.start = function (args) {
+  // read api file
   apiFilename = args[0]
   let processpath = process.cwd();
   apiFullPath = path.join(processpath, apiFilename)
@@ -17,8 +18,34 @@ exports.start = function(args) {
     console.log("make sure that you are running piriz on correct directory");
     process.exit(1);
   }
-
   api = require(apiFullPath)
-  console.log("TEST", api.TEST)
+
+  // create server info
+  info = {
+    'host': 'N/A',
+    'infoPort': 2679,
+    'dataPort': 2680,
+    'channelType': 'http-json',
+    'apiList': []
+  }
+  if (api.PIRIZ_SETTING) {
+    for (const key in api.PIRIZ_SETTING) {
+      if (info[key]) {
+        info[key] = api.PIRIZ_SETTING[key];
+      }
+    }
+  }
+
+  // update apiList
+  for (const key in api) {
+    if (typeof api[key] == "function") {
+      info.apiList.push(key)
+    }
+  }
+
+  // run info server
+  console.log(info)
+
+  // run target channel 
 
 }
